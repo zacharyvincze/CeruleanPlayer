7b3f9115eac03a144c0a7a9bc1ea7e013cb1d29c
zacharyvincze <zacharyvincze@gmail.com>
2022-09-05 01:36:23 -0400

    Updated version number to 1.3.0

--- src/Metadata.h
+++ src/Metadata.h
@@ -3,7 +3,7 @@
 #include <string>
 
 const unsigned int CERULEAN_VERSION_MAJOR = 1;
-const unsigned int CERULEAN_VERSION_MINOR = 2;
-const unsigned int CERULEAN_PATCH_NUMBER = 2;
+const unsigned int CERULEAN_VERSION_MINOR = 3;
+const unsigned int CERULEAN_PATCH_NUMBER = 0;
 
 const std::string CERULEAN_DEVELOPER_NAME = "Fetch";
